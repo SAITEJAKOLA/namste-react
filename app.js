5c2c629a96c41d053c52df22387488f8bcca7fd2
saitejakola <saiteja.01999@gmail.com>
2024-12-26 18:04:11 +0530

    Added resturant info

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import apiData from './resturant_db/res.json';
+const mediaURL = 'https://media-assets.swiggy.com/swiggy/image/upload/';
 /**
  * Header
  * - Logo
@@ -16,49 +18,84 @@ import ReactDOM from 'react-dom/client';
  * - Contact
  * */
 
-const Header = () =>{
-    return (
-        <div className='header'>
-            <div className='logo-container'>
-                <img className='logo' src='https://cdn.dribbble.com/users/7004524/screenshots/15197928/media/40944f27c2dcfaf14038f387b4e88c2d.jpg'/>
-            </div>
-            <div className='nav-items'>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>Cart</li>
-                </ul>   
-            </div>
-        </div>
-    )
-}
+/**
+ * Header Component
+ */
+const Header = () => (
+  <div className='header'>
+    <Logo />
+    <NavItems items={['Home', 'About', 'Contact', 'Cart']} />
+  </div>
+);
 
-const RestuarantCard = ()=>{
-    return (
-        <div className ='res-card'>
-            <img className='res-logo' src={require('./image_db/Babai.jpeg')} />
-        </div>
-    )
-}
-const Body = ()=>{
-    return (
-        <div className = 'body'>
-            <div className='search'> Search</div>
-            <div className = 'res-container'> 
-                <RestuarantCard/>
-                </div>
-        </div>
-    )
-}
-const AppLayout = ()=>{
-    return (
-        <div className='app'>
-            <Header/>
-            <Body/>
-        </div>
-    )
-}
+const Logo = () => (
+  <div className='logo-container'>
+    <img
+      className='logo'
+      src={`${mediaURL}fl_lossy,f_auto,q_auto,w_192,h_192/portal/c/logo_2022.png`}
+      alt='App Logo'
+    />
+  </div>
+);
+
+const NavItems = ({ items }) => (
+  <div className='nav-items'>
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  </div>
+);
+
+/**
+ * Body Component
+ */
+const Body = () => (
+  <div className='body'>
+    <SearchBar />
+    <RestaurantList restaurants={apiData.restaurants} />
+  </div>
+);
+
+const SearchBar = () => <div className='search'>Search</div>;
+
+const RestaurantList = ({ restaurants }) => (
+  <div className='res-container'>
+    {restaurants.map((restaurant) => (
+      <RestaurantCard key={restaurant.info.id} restaurant={restaurant.info} />
+    ))}
+  </div>
+);
+
+const RestaurantCard = ({ restaurant }) => {
+  const { name, cuisines, avgRating, sla, costForTwo, cloudinaryImageId } = restaurant;
+
+  return (
+    <div className='res-card'>
+      <img
+        className='res-logo'
+        src={`${mediaURL}fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
+        alt={name}
+      />
+      <h2>{name}</h2>
+      <h3>{cuisines.join(', ')}</h3>
+      <h4>{avgRating} Stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla.deliveryTime} mins</h4>
+    </div>
+  );
+};
+
+/**
+ * AppLayout Component
+ */
+const AppLayout = () => (
+  <div className='app'>
+    <Header />
+    <Body />
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppLayout/>);
+root.render(<AppLayout />);
